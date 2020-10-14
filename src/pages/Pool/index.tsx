91721c30c90737d26f9ae6edcff7a0d9ec6070aa
owl <owl@nyanswap.com>
2020-10-14 11:40:12 +0900

    chore: replace nyanswap/sdk

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useMemo } from 'react'
 import { ThemeContext } from 'styled-components'
-import { Pair } from '@uniswap/sdk'
+import { Pair } from '@nyanswap/sdk'
 import { Link } from 'react-router-dom'
 
 import Question from '../../components/QuestionHelper'
